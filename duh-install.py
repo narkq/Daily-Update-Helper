a6f08745c661cf5364cc99a43715f3ae4151529c
narkq <sladkov@gmail.com>
2011-11-17 00:01:41 +0400

    platform-dependent default config: linux

--- duh-install.py
+++ duh-install.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8
 # vim:fileencoding=utf-8
-import os, shutil
+import os, sys, shutil
 
 cfg_dir_path = '~/.duh'
 cfg_path = cfg_dir_path + '/config'
@@ -19,7 +19,9 @@ except IOError:
 	print 'Could not create my config dir'
 	exit()
 
+filename = '/config.default.' + sys.platform
+filename = os.path.abspath(os.path.dirname(__file__) + filename)
 try:
-	shutil.copyfile(os.path.abspath('./config.default'), cfg_real_path)
+	shutil.copyfile(filename, cfg_real_path)
 except IOError as error:
 	print 'Could not copy default config file to "', cfg_dir_real_path, '" (', error, ')'
