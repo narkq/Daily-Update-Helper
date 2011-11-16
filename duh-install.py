53615b21769e1da3a33acaeb83569c5f4cae18b7
narkq <sladkov@gmail.com>
2011-11-17 00:23:12 +0400

    added licence lol

--- duh-install.py
+++ duh-install.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8
 # vim:fileencoding=utf-8
+
+# ----------------------------------------------------------------------------
+# * "THE BEER-WARE LICENSE" (Revision 42):
+# * narkq@github.com wrote this file. As long as you retain this notice you
+# * can do whatever you want with this stuff. If we meet some day, and you think
+# * this stuff is worth it, you can buy me a beer in return
+# ----------------------------------------------------------------------------
+
 import os, sys, shutil
 
 cfg_dir_path = '~/.duh'
